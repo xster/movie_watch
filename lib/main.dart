f1193dd7e10f8e063ec7336ee98f73d12108be93
Xiao Yu <xster@google.com>
2017-11-13 18:22:46 -0800

    shows a screen with now playing movies

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,78 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+import 'tmdb/api.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(new App());
+
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Movie Watch',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        primarySwatch: Colors.green,
+        fontFamily: 'Avenir'),
+      home: new Scaffold(
+        appBar: new AppBar(title: const Text('Movie Watch')),
+        body: new NowPlaying()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class NowPlaying extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State<StatefulWidget> createState() => new NowPlayingState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class NowPlayingState extends State<NowPlaying> {
+  List<Map<String, dynamic>> movies;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    reload();
+  }
+
+  Future<Null> reload() async {
+    ApiResponse response = await MovieDatabase.instance.nowPlaying();
+    if (response.status != 200) { return; }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      movies = response.response['results'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    if (movies == null) {
+      return new Center(child: new CircularProgressIndicator());
+    }
+
+    return new Scrollbar(
+      child: new RefreshIndicator(
+        onRefresh: reload,
+        child: new GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 2.0,
+        crossAxisSpacing: 2.0,
+        childAspectRatio: 0.59,
+        children: movies.map<Widget>((Map<String, dynamic> movie) {
+          return new Card(
+            elevation: 3.0,
+            child: new Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 6.0),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(
+                    movie['title'],
+                    style: const TextStyle(fontSize: 16.0),
+                    maxLines: 2),
+                  new Image(
+                    image: new ApiPosterImage(movie['poster_path']))])));
+          }).toList()),
+        ));
   }
 }
--- lib/tmdb/api.dart
+++ lib/tmdb/api.dart
@@ -1,13 +1,58 @@
-import 'package:http/http.dart';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 import 'key.dart';
 
 class MovieDatabase {
-  MovieDatabase(this.apiKey);
+  MovieDatabase(this.apiKey) : assert(
+    apiKey != null && apiKey.isNotEmpty,
+    'You need a real API key in key.dart');
 
   final String apiKey;
 
   static MovieDatabase _instance;
   static MovieDatabase get instance => _instance ?? new MovieDatabase(developmentKey);
 
+  Future<ApiResponse> get(String path) async {
+    return await new ApiMethod(path, apiKey).get();
+  }
+
+  Future<ApiResponse> nowPlaying() async => get('movie/now_playing');
+}
+
+class ApiMethod {
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
+
+  ApiMethod(this.path, this.apiKey);
+
+  final String path;
+  final String apiKey;
+
+  Future<ApiResponse> get() async {
+    http.Response response = await http.get(new Uri.https(
+      'api.themoviedb.org',
+      '3/' + path,
+      {'api_key': apiKey}));
+
+    return new ApiResponse(response.statusCode, response.body);
+  }
+}
+
+class ApiResponse {
+  ApiResponse(this.status, this.responseString);
+
+  final int status;
+  final String responseString;
+
+  Map<String, dynamic> _json;
+  Map<String, dynamic> get response => _json ?? JSON.decode(responseString);
+}
+
+class ApiPosterImage extends NetworkImage {
+  ApiPosterImage(posterPath) : super(baseUrl + posterPath);
+
+  static const String baseUrl = 'https://image.tmdb.org/t/p/w500';
 }
